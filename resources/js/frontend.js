7b674760b3883120169ba489db4e843f75b2a524
Jake Petty <jake.petty@vexcon.net>
2019-05-15 23:51:18 -0500

    Fixed size issue on smaller screens

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -22,13 +22,7 @@ class Website {
     }
     setupServiceWorker() {
         if ('serviceWorker' in navigator) {
-           console.log("Will the service worker register?");
            navigator.serviceWorker.register('service-worker.js')
-             .then(function(reg){
-               console.log("Yes, it did.");
-            }).catch(function(err) {
-               console.log("No it didn't. This happened:", err)
-           });
         }
     }
     setupLightbox() {
